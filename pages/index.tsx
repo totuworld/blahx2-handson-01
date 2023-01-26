6189bd41ac4964dddf99341462547bc551c3f61f
totuworld <totuworld@gmail.com>
2023-01-26 20:40:48 +0900

    firebase 인증을 활용한 Google 로그인

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next';
 import { Box, Center, Flex, Heading } from '@chakra-ui/react';
+import { GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
 import { ServiceLayout } from '@/components/service_layout';
 import { GoogleLoginButton } from '@/components/google_login_button';
+import FirebaseClient from '@/models/firebase_client';
+
+const provider = new GoogleAuthProvider();
 
 const IndexPage: NextPage = function () {
   return (
@@ -13,7 +17,17 @@ const IndexPage: NextPage = function () {
         </Flex>
       </Box>
       <Center mt="20">
-        <GoogleLoginButton />
+        <GoogleLoginButton
+          onClick={() => {
+            signInWithPopup(FirebaseClient.getInstance().Auth, provider)
+              .then((result) => {
+                console.info(result.user);
+              })
+              .catch((error) => {
+                console.error(error);
+              });
+          }}
+        />
       </Center>
     </ServiceLayout>
   );
