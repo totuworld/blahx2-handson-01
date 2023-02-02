09d861657b579fd414c11af0d9a7d24d5b936053
totuworld <totuworld@gmail.com>
2023-02-02 21:13:14 +0900

    사용자 모델 추가

---
+++ models/member/member.model.ts
@@ -0,0 +1,46 @@
+import FirebaseAdmin from '../firebase_admin';
+import { InAuthUser } from '../in_auth_info';
+
+const MEMBER_COL = 'members';
+const SCR_NAME_COL = 'screen_names';
+
+async function add({
+  uid,
+  email,
+  displayName,
+  photoURL,
+}: InAuthUser): Promise<{ result: true; id: string } | { result: false; message: string }> {
+  try {
+    const screenName = (email ?? '').replace('@gmail.com', '');
+    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
+      const memberRef = FirebaseAdmin.getInstance().Firebase.collection(MEMBER_COL).doc(uid);
+      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection(SCR_NAME_COL).doc(screenName);
+      const memberDoc = await transaction.get(memberRef);
+      if (memberDoc.exists) {
+        return false;
+      }
+      const addData = {
+        uid,
+        email: email ?? '',
+        displayName: displayName ?? '',
+        photoURL: photoURL ?? '',
+      };
+      await transaction.set(memberRef, addData);
+      await transaction.set(screenNameRef, addData);
+      return true;
+    });
+    if (addResult === false) {
+      return { result: true, id: uid };
+    }
+    return { result: true, id: uid };
+  } catch (err) {
+    console.error(err);
+    return { result: false, message: '서버 에러' };
+  }
+}
+
+const MemberModel = {
+  add,
+};
+
+export default MemberModel;
--- pages/api/members.add.ts
+++ pages/api/members.add.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import { NextApiRequest, NextApiResponse } from 'next';
-import FirebaseAdmin from '@/models/firebase_admin';
+import MemberModel from '@/models/member/member.model';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { uid, email, displayName, photoURL } = req.body;
@@ -14,31 +14,9 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     return res.status(400).json({ result: false, message: 'email이 누락되었습니다' });
   }
 
-  try {
-    const screenName = (email as string).replace('@gmail.com', '');
-    const addResult = await FirebaseAdmin.getInstance().Firebase.runTransaction(async (transaction) => {
-      const memberRef = FirebaseAdmin.getInstance().Firebase.collection('members').doc(uid);
-      const screenNameRef = FirebaseAdmin.getInstance().Firebase.collection('screen_names').doc(screenName);
-      const memberDoc = await transaction.get(memberRef);
-      if (memberDoc.exists) {
-        return false;
-      }
-      const addData = {
-        uid,
-        email: email ?? '',
-        displayName: displayName ?? '',
-        photoURL: photoURL ?? '',
-      };
-      await transaction.set(memberRef, addData);
-      await transaction.set(screenNameRef, addData);
-      return true;
-    });
-    if (addResult === false) {
-      return res.status(201).json({ result: true, id: uid });
-    }
-    return res.status(200).json({ result: true, id: uid });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ result: false });
+  const addResult = await MemberModel.add({ uid, email, displayName, photoURL });
+  if (addResult.result === true) {
+    return res.status(200).json(addResult);
   }
+  res.status(500).json(addResult);
 }
