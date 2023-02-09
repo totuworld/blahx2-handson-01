4e533610402d682058f946ef7c2979e727e182da
totuworld <totuworld@gmail.com>
2023-02-09 21:13:39 +0900

    질문 입력 컴포넌트 수정

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,8 +1,9 @@
-import { Avatar, Box, Button, Flex, Text, Textarea, useToast } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, FormControl, FormLabel, Switch, Text, Textarea, useToast } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import ResizeTextArea from 'react-textarea-autosize';
 import { useState } from 'react';
 import { ServiceLayout } from '@/components/service_layout';
+import { useAuth } from '@/contexts/auth_user.context';
 
 const userInfo = {
   uid: 'test',
@@ -13,7 +14,9 @@ const userInfo = {
 
 const UserHomePage: NextPage = function () {
   const [message, setMessage] = useState('');
+  const [isAnonymous, setAnonymous] = useState(true);
   const toast = useToast();
+  const { authUser } = useAuth();
   return (
     <ServiceLayout title="user home" maxH="100vh" backgroundColor="gray.50">
       <Box maxW="md" mx="auto" pt="6">
@@ -28,7 +31,7 @@ const UserHomePage: NextPage = function () {
         </Box>
         <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
           <Flex align="center" p="2">
-            <Avatar size="xs" src="/user.png" mr="2" />
+            <Avatar size="xs" src={isAnonymous ? '/user.png' : authUser?.photoURL ?? '/user.png'} mr="2" />
             <Textarea
               bg="gray.100"
               border="none"
@@ -65,6 +68,25 @@ const UserHomePage: NextPage = function () {
               등록
             </Button>
           </Flex>
+          <FormControl display="flex" alignItems="center" mt="1" mx="2" pb="2">
+            <Switch
+              size="sm"
+              colorScheme="orange"
+              id="anonymous"
+              mr="1"
+              isChecked={isAnonymous}
+              onChange={() => {
+                if (authUser === null) {
+                  toast({ title: '로그인이 필요합니다', position: 'top-right' });
+                  return;
+                }
+                setAnonymous((prev) => !prev);
+              }}
+            />
+            <FormLabel htmlFor="anonymous" mb="0" fontSize="xx-small">
+              Anonymous
+            </FormLabel>
+          </FormControl>
         </Box>
       </Box>
     </ServiceLayout>
