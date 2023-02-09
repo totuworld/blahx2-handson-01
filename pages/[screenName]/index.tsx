ab1b14a7c604e37e204507c088d2e2bad0eed2c6
totuworld <totuworld@gmail.com>
2023-02-09 20:46:39 +0900

    질문 입력 컴포넌트 제작 - 1

--- pages/[screenName]/index.tsx
+++ pages/[screenName]/index.tsx
@@ -1,5 +1,7 @@
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
+import { Avatar, Box, Button, Flex, Text, Textarea } from '@chakra-ui/react';
 import { NextPage } from 'next';
+import ResizeTextArea from 'react-textarea-autosize';
+import { useState } from 'react';
 import { ServiceLayout } from '@/components/service_layout';
 
 const userInfo = {
@@ -10,6 +12,7 @@ const userInfo = {
 };
 
 const UserHomePage: NextPage = function () {
+  const [message, setMessage] = useState('');
   return (
     <ServiceLayout title="user home" maxH="100vh" backgroundColor="gray.50">
       <Box maxW="md" mx="auto" pt="6">
@@ -22,7 +25,42 @@ const UserHomePage: NextPage = function () {
             </Flex>
           </Flex>
         </Box>
-        test 123
+        <Box borderWidth="1px" borderRadius="lg" overflow="hidden" mb="2" bg="white">
+          <Flex align="center" p="2">
+            <Avatar size="xs" src="/user.png" mr="2" />
+            <Textarea
+              bg="gray.100"
+              border="none"
+              placeholder="무엇이 궁금한가요?"
+              resize="none"
+              minH="unset"
+              overflow="hidden"
+              fontSize="xs"
+              maxRows={7}
+              as={ResizeTextArea}
+              value={message}
+              onChange={(e) => {
+                if (e.currentTarget.value) {
+                  const lineCount = e.currentTarget.value.match(/[^\n]*\n[^\n]*/gi)?.length ?? 0;
+                  if (lineCount > 7) {
+                    return;
+                  }
+                }
+                setMessage(e.currentTarget.value);
+              }}
+            />
+            <Button
+              disabled={message.length === 0}
+              bgColor="#FFB86C"
+              color="white"
+              colorScheme="yellow"
+              variant="solid"
+              size="sm"
+            >
+              등록
+            </Button>
+          </Flex>
+        </Box>
       </Box>
     </ServiceLayout>
   );
