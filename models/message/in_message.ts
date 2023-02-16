cd0ed02512a12191f556227941a90e35953d6169
totuworld <totuworld@gmail.com>
2023-02-16 20:28:40 +0900

    메시지 목록 조회 API 구현

--- models/message/in_message.ts
+++ models/message/in_message.ts
@@ -1,13 +1,22 @@
-export interface InMessage {
+import { firestore } from 'firebase-admin';
+
+export interface MessageBase {
   id: string;
   /** 사용자가 남긴 질문 */
   message: string;
   /** 댓글 */
   reply?: string;
-  createAt: string;
-  replayAt?: string;
   author?: {
     displayName: string;
     photoURL?: string;
   };
+}
+export interface InMessage extends MessageBase {
+  createAt: string;
+  replayAt?: string;
+}
+
+export interface InMessageServer {
+  createAt: firestore.Timestamp;
+  replayAt?: firestore.Timestamp;
 }
--- models/message/message.model.ts
+++ models/message/message.model.ts
@@ -1,6 +1,7 @@
 import { firestore } from 'firebase-admin';
 import CustomServerError from '@/controllers/error/custom_server_error';
 import FirebaseAdmin from '../firebase_admin';
+import { InMessage, InMessageServer } from './in_message';
 
 const MEMBER_COL = 'members';
 const MSG_COL = 'messages';
@@ -45,8 +46,33 @@ async function post({
   });
 }
 
+async function list({ uid }: { uid: string }) {
+  const memberRef = Firestore.collection(MEMBER_COL).doc(uid);
+  const listData = await Firestore.runTransaction(async (transaction) => {
+    const memberDoc = await transaction.get(memberRef);
+    if (memberDoc.exists === false) {
+      throw new CustomServerError({ statusCode: 400, message: '존재하지않는 사용자입니다' });
+    }
+    const messageCol = memberRef.collection(MSG_COL);
+    const messageColDoc = await transaction.get(messageCol);
+    const data = messageColDoc.docs.map((mv) => {
+      const docData = mv.data() as Omit<InMessageServer, 'id'>;
+      const returnData = {
+        ...docData,
+        id: mv.id,
+        createAt: docData.createAt.toDate().toISOString(),
+        replayAt: docData.replayAt ? docData.replayAt.toDate().toISOString() : undefined,
+      } as InMessage;
+      return returnData;
+    });
+    return data;
+  });
+  return listData;
+}
+
 const MessageModel = {
   post,
+  list,
 };
 
 export default MessageModel;
